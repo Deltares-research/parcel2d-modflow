a8c0d675562de4b4eb92334028def5ef924acaeb
smknaake <bas.knaake@deltares.nl>
2026-08-24 14:52:53 +0200

    Pseudo setup for ditch level interpolation

---
+++ src/parcel2d_modflow/io/database.py
@@ -0,0 +1,8 @@
+def initialize_database():
+    # Initializes database connection with SOMERS DB
+    pass
+
+
+def load_ditches():
+    # Loads ditches from SOMERS DB
+    pass
---
+++ src/parcel2d_modflow/preprocessing/ditch_levels.py
@@ -0,0 +1,12 @@
+import xarray as xr
+
+
+def interpolate_ditch_levels():
+    # Interpoalates the ditch levels download from the database per parcel
+    pass
+
+
+def create_preset_ditch_levels() -> xr.DataArray:
+    # Creates a preset ditch level for each parcel based on the average ditch level
+    # Creates a DataArray with dimensions (name, time)
+    pass
